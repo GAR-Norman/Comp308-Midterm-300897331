ad60cf9f4dedde98a908b274b572ac8521806e69
G <gabeanthonynorman@outlook.com>
2019-02-25 15:51:54 -0500

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,9 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books308"
+  //Remote MongoDB deployment -> let 
+  //"URI": "mongodb://thomas:123456@ds161059.mlab.com:61059/books308"
+
+  //My MLab URI (I used the same db instance as my week 04 )
+  "URI": "mongodb://Tom:a123456@ds060977.mlab.com:60977/gabenorman"
+};
